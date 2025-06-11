86cd564b6aa0c69f64dc1ec41482e2212ce41dd0
Julia Canalle <julia.canalle4@gmail.com>
2025-06-11 14:06:54 -0300

    #28 - Descrição -> #28 - feature - Funcionalidade para uma empresa debitar da conta de um colaborador.

--- src/main/java/io/github/juliacanalle/creditointernoapi/service/ContaService.java
+++ src/main/java/io/github/juliacanalle/creditointernoapi/service/ContaService.java
@@ -41,6 +41,33 @@ public class ContaService {
         if (empresaDoColaborador != empresa) {
             throw new IllegalArgumentException("Esse colaborador não pertence a empresa " + empresa + ".");
         }
-         colaborador.getConta().creditar(valor);
+        colaborador.getConta().creditar(valor);
+    }
+
+    public void debitarConta(BigDecimal valor, String cnpj, String cpf) {
+        var empresa = empresaRepository.findByCnpj(cnpj);
+        if (empresa == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        if (!empresa.isAtivo()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        var colaborador = colaboradorRepository.findByCpf(cpf);
+        if (colaborador == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        if (!colaborador.isAtivo()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        var empresaDoColaborador = colaborador.getEmpresa();
+        if (empresaDoColaborador != empresa) {
+            throw new IllegalArgumentException("Esse colaborador não pertence a empresa " + empresa + ".");
+        }
+        colaborador.getConta().debitar(valor);
     }
 }
+
