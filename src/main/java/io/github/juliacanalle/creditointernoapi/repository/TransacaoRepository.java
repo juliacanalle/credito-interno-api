4ef8a0019ded8605364d5eca67a50e1b8be53469
Julia Canalle <julia.canalle4@gmail.com>
2025-06-17 11:18:35 -0300

    Iniciando a paginação, ordenação e filtro por datas.

--- src/main/java/io/github/juliacanalle/creditointernoapi/repository/TransacaoRepository.java
+++ src/main/java/io/github/juliacanalle/creditointernoapi/repository/TransacaoRepository.java
@@ -3,11 +3,16 @@ package io.github.juliacanalle.creditointernoapi.repository;
 import io.github.juliacanalle.creditointernoapi.model.Colaborador;
 import io.github.juliacanalle.creditointernoapi.model.Conta;
 import io.github.juliacanalle.creditointernoapi.model.Transacao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 
     List<Transacao> findByConta(Conta conta);
+
+    Page<Transacao> findByContaAndDate (Conta conta, Pageable pageable, LocalDateTime inicio, LocalDateTime fim);
 }
